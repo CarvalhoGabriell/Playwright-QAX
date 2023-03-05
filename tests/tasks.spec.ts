a6a78d63caad79254627f1a63529e1d218f36489
CarvalhoGabriel <gabriel.f.ext@tooseguros.com.br>
2023-03-04 23:33:13 -0300

    Finalizando projeto com testes de Atualizar, Remover, Cadastrar tasks

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -6,52 +6,89 @@ import {TaskModel} from './fixtures/task.model'
 import { TasksPage } from './Pages/tasks'; 
 import {deleteTaskNameByHelper, postNewTask} from './TypeScript/Helpers'
 
-// cada test() representa um cenário de teste
-test('Deve cadastrar uma nova Task', async({page, request}) => {
-
-    let newTask: TaskModel = {
-        name: "Concluir os cursos da QAX",
-        is_done: false
-    }
-    //delete
-    deleteTaskNameByHelper(request, newTask.name)
-
-    const taskPage: TasksPage = new TasksPage(page)
-    await taskPage.goURL();
-    await taskPage.create(newTask)
-
-    await taskPage.shouldHaveTextTask(newTask.name)
-});
-
-test('Não deve cadastrar tasks duplicadas', async({page, request})=> {
-
-    const payload: TaskModel = {
-        name: "Concluir testes para a prova",
-        is_done: false
-    }
-    //delete
-    deleteTaskNameByHelper(request, payload.name)
-    //post
-    postNewTask(request, payload)
-
-    const taskPage: TasksPage = new TasksPage(page)
-    await taskPage.goURL();
-    await taskPage.create(payload)
-    await taskPage.expectModalError('Task already exists!')
+let taskPage: TasksPage
+test.beforeEach(({page})=> {
+    taskPage = new TasksPage(page)
 })
 
-
-test('Deve validar o campo obrigatório',async ({page}) => {
+test.describe('Cadastro Task', () => {
+    // cada test() representa um cenário de teste
+    test('Deve cadastrar uma nova Task', async({ request}) => {
     
-    const payload: TaskModel = {
-        name: '',
-        is_done: false
-    }
+        let newTask: TaskModel = {
+            name: "Concluir os cursos da QAX",
+            is_done: false
+        }
+        //delete
+        deleteTaskNameByHelper(request, newTask.name)
 
-    const taskPage: TasksPage = new TasksPage(page)
-    await taskPage.goURL();
-    await taskPage.create(payload)
+        await taskPage.goURL();
+        //const countBeforeNewTask = page.locator('xpath=//div[text()="Created Tasks"]//span')
+        await taskPage.create(newTask)
+        await taskPage.shouldHaveTextTask(newTask.name)
+    });
+    
+    test('Não deve cadastrar tasks duplicadas', async({ request})=> {
+    
+        const payload: TaskModel = {
+            name: "Concluir testes para a prova",
+            is_done: false
+        }
+        //post
+        postNewTask(request, payload)
+        await taskPage.goURL();
+        await taskPage.create(payload)
+        await taskPage.expectModalError('Task already exists!')
+    })
+    
+    
+    test('Deve validar o campo obrigatório',async ({page}) => {
+        
+        const payload: TaskModel = {
+            name: '',
+            is_done: false
+        }
+    
+        await taskPage.goURL();
+        await taskPage.create(payload)
+    
+        const validateMsg = await taskPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
+        expect(validateMsg).toEqual('This is a required field')
+    })
 
-    const validateMsg = await taskPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
-    expect(validateMsg).toEqual('This is a required field')
+})
+
+test.describe('Atualização', () => {
+    test('Deve concluir tarefa cadastrada',async ({ request}) => {
+        
+        const newTask: TaskModel = {
+            name:'Pagar emprestimo',
+            is_done: false
+        }
+    
+        deleteTaskNameByHelper(request, newTask.name)
+        postNewTask(request, newTask)
+    
+        await taskPage.goURL();
+        await taskPage.toggleDoneTask(newTask.name)
+        await taskPage.shouldBeDone(newTask.name)
+    })
+
+})
+
+test.describe('Exclusão', ()=> {
+    test('Deve remover uma task', async({ request}) => {
+        
+        const payloadDel: TaskModel = {
+            name:'Criar README',
+            is_done: false
+        }
+    
+        deleteTaskNameByHelper(request, payloadDel.name)
+        postNewTask(request, payloadDel)
+
+        await taskPage.goURL()
+        await taskPage.removeTask(payloadDel.name)
+        await taskPage.shouldNotExistTask(payloadDel.name)
+    })
 })
